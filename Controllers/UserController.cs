e859b2053bfd2ec1d85630942e89a7d2c87c15af
Stitch <st.van.eekelen@gmail.com>
2022-01-16 22:01:16 +0100

    Sadly still not fixed on live.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SoftwareFullComponent.UserComponent;
+using SoftwareFullComponent.UserComponent.DTO;
 
 namespace SoftwareFullComponents.LicenseComponent.Controllers
 {
@@ -6,6 +10,16 @@ namespace SoftwareFullComponents.LicenseComponent.Controllers
     [Route("api/[controller]")]
     public class UserController : Controller
     {
+        private readonly UserLogicInterface _userLogic;
+        public UserController(UserLogicInterface userLogic)
+        {
+            _userLogic = userLogic;
+        }
         
+        [HttpGet("")]
+        public async Task<IEnumerable<UserRead>> GetUsers()
+        {
+            return await _userLogic.GetUsers();
+        }
     }
 }
